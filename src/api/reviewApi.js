d4eea7800cf4bbdde3bb0c8595685f7a98df1be9
Ruzbihan <ruzbihanmzm@gmail.com>
2024-11-25 22:58:33 +0530

    review api

---
+++ src/api/reviewApi.js
@@ -0,0 +1,7 @@
+import { apiService } from "../utils/api";
+
+export const fetchAllReviews = () => apiService.get("/reviews");
+export const fetchReviewsByUser = () => apiService.get("/reviews/user");
+export const fetchReviewsForBook = (bookId) => apiService.get(`/reviews/book/${bookId}`);
+export const createReview = (reviewData) => apiService.post("/reviews", reviewData);
+export const deleteReview = (bookId) => apiService.delete(`/reviews/${bookId}`);
