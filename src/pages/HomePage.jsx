54de608e5b04a6df4ef4f453b3f2c481f3effaca
Ruzbihan <ruzbihanmzm@gmail.com>
2024-11-27 03:18:55 +0530

    Home Page UI and Function Implementation

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,40 +1,83 @@
 import React, { useEffect, useState } from "react";
-import {
-  Grid2 as Grid,
-  Typography,
-  Container,
-  Card,
-  CardMedia,
-  CardContent,
-} from "@mui/material";
+import { Grid2 as Grid, Typography } from "@mui/material";
 import BookCard from "../components/Books/BookCard";
 import { fetchBooks } from "../api/bookApi";
+import { useNavigate } from "react-router-dom";
+import { addBookToLibrary, fetchUserLibrary } from "../api/libraryApi";
+import { showSweetAlert } from "../utils/sweetAlertUtil";
+import Loader from "../components/Layout/Loader";
+import { handleApiError } from "../utils/handleApiError";
 
 const HomePage = () => {
   const [books, setBooks] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const getBooks = async () => {
       try {
-        const response = await fetchBooks();
-        const booksWithImages = response.data.map((book) => {
+        const [bookResponse, libraryResponse] = await Promise.all([
+          fetchBooks(),
+          fetchUserLibrary(),
+        ]);
+
+        const booksWithImages = bookResponse.data.map((book) => {
           const randomImage = Math.floor(Math.random() * 1000) + 1;
           return {
             ...book,
             coverImage: `https://picsum.photos/150/200?random=${randomImage}`,
           };
         });
-        setBooks(booksWithImages);
+
+        const libraryBookIds = libraryResponse.data.map(
+          (libraryBook) => libraryBook.BookId
+        );
+
+        setBooks(
+          booksWithImages.map((book) => ({
+            ...book,
+            inLibrary: libraryBookIds.includes(book.id),
+          }))
+        );
       } catch (err) {
         console.error(err.message);
+        handleApiError(err);
       }
     };
 
     getBooks();
   }, []);
 
+  const handleViewDetails = (id) => {
+    navigate(`/books/${id}`);
+  };
+
+  const onAddToLibrary = async (bookId) => {
+    try {
+      await addBookToLibrary(bookId);
+      showSweetAlert({
+        icon: "success",
+        title: "Book Added",
+        text: "The book added to your library!",
+      });
+
+      // Update the book's inLibrary status
+      setBooks((prevBooks) =>
+        prevBooks.map((book) =>
+          book.id === bookId ? { ...book, inLibrary: true } : book
+        )
+      );
+    } catch (err) {
+      console.error(err.message);
+      handleApiError(err, "info", "Already Exists");
+    }
+  };
+
+  if (!books) {
+    return <Loader />;
+  }
+
   return (
-    <div className="px-10 md:px-28">
+    <div className="p-10 md:px-28">
       <Typography variant="h4" gutterBottom align="center">
         Welcome to the Book Recommendation System
       </Typography>
@@ -44,7 +87,12 @@ const HomePage = () => {
       <Grid container spacing={3}>
         {books.map((book) => (
           <Grid item key={book.id}>
-            <BookCard book={book} />
+            <BookCard
+              book={book}
+              onView={handleViewDetails}
+              onAddToLibrary={onAddToLibrary}
+              inLibrary={book.inLibrary}
+            />
           </Grid>
         ))}
       </Grid>
