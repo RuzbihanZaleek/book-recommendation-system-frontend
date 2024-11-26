bad6685cef540e751cf6c49167b29b5d9608c210
Ruzbihan <ruzbihanmzm@gmail.com>
2024-11-27 03:20:56 +0530

    Reviews UI and Functions Implementation

---
+++ src/components/Reviews/ReviewCarousel.jsx
@@ -0,0 +1,56 @@
+import { Typography, Card, Rating, Box } from "@mui/material";
+import Carousel from "react-multi-carousel";
+
+const ReviewCarousel = ({ reviews }) => {
+    console.log(reviews);
+  const responsive = {
+    desktop: { breakpoint: { max: 3000, min: 1024 }, items: 3 },
+    tablet: { breakpoint: { max: 1024, min: 464 }, items: 2 },
+    mobile: { breakpoint: { max: 464, min: 0 }, items: 1 },
+  };
+
+  return (
+    <Box>
+      <Typography variant="h5" sx={{ marginBottom: 2 }} fontStyle="italic">
+        Reviews
+      </Typography>
+      <Carousel
+        responsive={responsive}
+        infinite
+        autoPlay
+        autoPlaySpeed={2000}
+        pauseOnHover
+        arrows={false}
+      >
+        {reviews.map((review) => (
+          <Card
+            key={review.id}
+            sx={{
+              padding: 2,
+              margin: 2,
+              boxShadow: 3,
+              borderRadius: 2,
+              textAlign: "left",
+            }}
+          >
+            <Typography>{review.content}</Typography>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                marginTop: 1,
+              }}
+            >
+              <Rating value={review.rating} readOnly precision={0.5} />
+              <Typography variant="subtitle2" color="textSecondary">
+                - {review.User?.username}
+              </Typography>
+            </Box>
+          </Card>
+        ))}
+      </Carousel>
+    </Box>
+  );
+};
+
+export default ReviewCarousel;
