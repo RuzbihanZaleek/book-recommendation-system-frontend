a9ead3ae707eea7c73cd6590036bbf582271f3d6
Ruzbihan <ruzbihanmzm@gmail.com>
2024-11-28 21:13:53 +0530

    navigating user to login page if the token has expired

--- src/utils/api.js
+++ src/utils/api.js
@@ -31,6 +31,11 @@ apiClient.interceptors.response.use(
     }
   },
   (error) => {
+    if (error.response?.status === 401) {
+      // Token has expired or is invalid
+      localStorage.removeItem("authToken");
+      window.location.href = "/login";
+    }
     return Promise.reject(error.response.data);
   }
 );
